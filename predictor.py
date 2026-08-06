2c611956e2277b3b586e9235249da3ece6c2a731
nunovinhas-creator <278368112+nunovinhas-creator@users.noreply.github.com>
2026-08-06 12:26:58 +0100

    Create predictor.py

---
+++ predictor.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from adjusted_xg import calcular_adjusted_xg
+from api import (
+    obter_prediction,
+    obter_odds,
+    obter_stats_evento,
+    obter_h2h
+)
+
+
+def monte_carlo(lambda_home, lambda_away, simulations=100000):
+    pass
+
+
+def calcular_value(probabilidade, odd):
+    pass
+
+
+def calcular_confidence(mc_prob, bsd_prob):
+    pass
+
+
+def calcular_kelly(probabilidade, odd):
+    pass
+
+
+def prever(match):
+    pass
